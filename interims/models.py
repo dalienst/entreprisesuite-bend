1580f326cbb4f7426195d4fc6621fb011b03a1d4
dalienst <oduordalienst@gmail.com>
2024-06-03 20:26:15 +0300

    add interim app

--- interims/models.py
+++ interims/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from users.abstracts import UniversalIdModel, TimeStampedModel
+from milestones.models import Milestone
+
+User = get_user_model()
+
+# class InterimInvoice(UniversalIdModel, TimeStampedModel):
+#     """
+#     An invoice to be sent out to request payment for completed milestones
+#     """
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="interim_invoices")
+#     milestone = models
